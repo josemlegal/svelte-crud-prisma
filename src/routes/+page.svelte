177cf113e0c8ac5a62f4f4c044882fc8af4876b0
josemlegal <josemlegal97@gmail.com>
2023-02-24 14:41:39 -0300

    Add the pages and the first create action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid">
+	<div>
+		<h2>Articles:</h2>
+		<article>
+			<header />
+			<p />
+			<form action="?/deleteArticle&id=" method="POST">
+				<button type="submit" class="outline secondary">Delete Article</button>
+			</form>
+			<a href="/" role="button" class="outline constrast" style="width: 100%;">Edit Article</a>
+		</article>
+	</div>
+	<form action="?/createArticle" method="POST">
+		<h3>New Article</h3>
+		<label for="title"> Title </label>
+		<input type="text" id="title" name="title" />
+		<label for="title"> Title </label>
+		<textarea id="content" name="content" rows={5} />
+		<button type="submit">Add Article</button>
+	</form>
+</div>
